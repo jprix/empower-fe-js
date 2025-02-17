1e33b3a197769c022a9256b0c599abae13175a09
jprix <parisijason@gmail.com>
2025-02-17 17:15:34 -0500

    removed Dynamic Context

--- pages/account.jsx
+++ pages/account.jsx
@@ -1,10 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-  useDynamicContext,
-  useUserWallets,
-  useRpcProviders,
-  useTokenBalances,
-} from "@dynamic-labs/sdk-react-core";
+
 import {
   Box,
   Typography,
@@ -18,79 +13,11 @@ import Header from "../components/Header";
 import Verifications from "../components/Verifications";
 
 export default function Account() {
-  const { user, primaryWallet } = useDynamicContext();
-  const userWallets = useUserWallets();
   const { defaultProvider } = useRpcProviders(evmProvidersSelector); // Get the EVM provider
   const [selectedCredential, setSelectedCredential] = useState("");
   const [currentAccount, setCurrentAccount] = useState({});
   const [balance, setBalance] = useState(null);
   const [usdBalance, setUsdBalance] = useState(null);
-  const ETH_TO_USD_API =
-    "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd";
-  const [ethToUsdRate, setEthToUsdRate] = useState("");
-  const [ethBalance, setEthBalance] = useState("");
-  const [loading, setLoading] = useState(false); // Loading state
-  const { tokenBalances, isLoading, isError, error } = useTokenBalances();
-
-  console.log("tokenBalances", tokenBalances);
-  useEffect(() => {
-    fetchEthToUsdRate();
-  }, []);
-
-  const fetchEthToUsdRate = async () => {
-    try {
-      const response = await fetch(ETH_TO_USD_API);
-      const data = await response.json();
-      setEthToUsdRate(data.ethereum.usd);
-    } catch (error) {
-      console.error("Error fetching ETH to USD rate:", error);
-      return null;
-    }
-  };
-
-  const handleCredentialChange = (event) => {
-    const account = user?.verifiedCredentials.find(
-      (vc) => vc.id === event.target.value
-    );
-    console.log("account", account);
-
-    setCurrentAccount(account);
-    setSelectedCredential(event.target.value);
-    setBalance(null); // Reset balance when a new account is selected
-    setUsdBalance(null); // Reset USD balance
-  };
-
-  const signMessage = async () => {
-    const wallet = userWallets.find((w) => w.id === selectedCredential);
-    if (!wallet) return;
-
-    const message = "You are awesome";
-    try {
-      const signature = await wallet.signMessage(message);
-      console.log("Signature:", signature);
-      alert("Message signed! Check the console for the signature.");
-    } catch (error) {
-      console.error("Error signing message:", error);
-    }
-  };
-
-  const sendTransaction = async () => {
-    const wallet = userWallets.find((w) => w.id === selectedCredential);
-    if (!wallet) return;
-
-    const ethValue = "0.0000000000001"; // Value in ETH
-    const weiValue = BigInt(Number(ethValue) * 10 ** 18);
-
-    try {
-      const tx = await wallet.sendTransaction({
-        to: "0xcC90c7c3E3Ad6e4E6bd8CF4fB10D09edC20a9506",
-        value: weiValue,
-      });
-      console.log("tx", tx);
-    } catch (error) {
-      console.error("Transaction failed:", error);
-    }
-  };
 
   return (
     <>
@@ -104,49 +31,42 @@ export default function Account() {
           >
             Profile
           </Typography>
-          {user ? (
-            <>
-              <List>
-                <ListItem>
-                  <Typography variant="body1">
-                    <strong>User ID:</strong> {user.userId}
-                  </Typography>
-                </ListItem>
-                <ListItem>
-                  <Typography variant="body1">
-                    <strong>Email:</strong> {user.email}
-                  </Typography>
-                </ListItem>
-                <ListItem>
-                  <Typography variant="body1">
-                    <strong>Session ID:</strong> {user.sessionId}
-                  </Typography>
-                </ListItem>
-                <ListItem>
-                  <Typography variant="body1">
-                    <strong>Environment ID:</strong> {user.environmentId}
-                  </Typography>
-                </ListItem>
-                <ListItem>
-                  <Typography variant="body1">
-                    <strong>New User:</strong> {user.newUser ? "Yes" : "No"}
-                  </Typography>
-                </ListItem>
-                <ListItem>
-                  <Typography variant="body1">
-                    <strong>Last Verified Credential ID:</strong>{" "}
-                    {user?.lastVerifiedCredentialId}
-                  </Typography>
-                </ListItem>
-              </List>
-              {/* Account Dropdown with Card */}
-              <Verifications />
-            </>
-          ) : (
-            <Typography variant="body1">
-              No user information available. Please log in.
-            </Typography>
-          )}
+          <>
+            <List>
+              <ListItem>
+                <Typography variant="body1">
+                  <strong>User ID:</strong> 12345
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <Typography variant="body1">
+                  <strong>Email:</strong> email
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <Typography variant="body1">
+                  <strong>Session ID:</strong> 12345
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <Typography variant="body1">
+                  <strong>Environment ID:</strong> 12345
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <Typography variant="body1">
+                  <strong>New User:</strong>
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <Typography variant="body1">
+                  <strong>Last Verified Credential ID:</strong> 12345
+                </Typography>
+              </ListItem>
+            </List>
+            {/* Account Dropdown with Card */}
+            <Verifications />
+          </>
         </Card>
       </Box>
     </>
